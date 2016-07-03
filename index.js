c95406f3c3169d5f41222972a6a12cd0581ffc00
Philippe CHARRIERE <k33g@github.com>
2016-07-03 18:35:43 +0200

    🚀

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+function summer (sumVal) {
+  return function (toAdd) {
+    return sumVal + toAdd
+  }
+}
+
+module.exports = summer;
---
+++ tests/yo-tests.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var summer = require('../');
+var assert = require('chai').assert;
+
+describe("yo it's cold", () => {
+  var winter = summer(-40);
+
+  it("should return 0", () => {
+    assert.equal(winter(40), 0)
+  });
+});
